5ca33f630d61bbb9e57a5d8703957985a5a9c2c5
Angel <angel.cayatopa@unmsm.edu.pe>
2020-02-20 01:00:10 -0500

    vista de categorias hecha

--- ferresol/administracion/views.py
+++ ferresol/administracion/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
@@ -7,3 +7,47 @@ from django.shortcuts import render
 def Principal(request):
 
     return render(request,'index.html')
+
+
+def CategoriaView(request):
+
+    productos = Producto.objects.filter(tipo = 'Categoria')
+    titulo = 'CATEGORIAS'
+    link = 'subcategoria'
+    return render(request,
+                  'administracion/categoria.html',
+                  context={
+                      'productos':productos,
+                      'titulo':titulo,
+                      'link':link,
+                  })
+
+
+
+def SubcategoriaView(request,id):
+
+    productos = Producto.objects.filter(id_producto = id)
+    titulo = 'SUBCATEGORIAS'
+    link = 'producto'
+
+    return render(request,
+                  'administracion/categoria.html',
+                  context={
+                      'productos': productos,
+                      'titulo': titulo,
+                      'link': link,
+                  })
+
+def ProductoView(request,id):
+    productos = Producto.objects.filter(id_producto=id)
+    titulo = 'PRODUCTOS'
+
+    link = 'producto'
+
+    return render(request,
+                  'administracion/categoria.html',
+                  context={
+                      'productos': productos,
+                      'titulo': titulo,
+                      'link': link,
+                  })
--- ferresol/ferresol/urls.py
+++ ferresol/ferresol/urls.py
@@ -10,6 +10,12 @@ from administracion import views as admi_v
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('principal/',admi_v.Principal,name='principal')
+    path('principal/',admi_v.Principal,name='principal'),
+
+    path('categoria/',admi_v.CategoriaView,name='categoria'),
+
+    path('subcategoria/<int:id>',admi_v.SubcategoriaView,name='subcategoria'),
+
+    path('producto/<int:id>',admi_v.ProductoView,name='producto'),
 
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
