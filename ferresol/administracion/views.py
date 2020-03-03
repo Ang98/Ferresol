6028fb8a22f7623bf27373271154cd32c29acfd5
Cesar kennedy rousseau arzapalo caldas <cesar.arzapalo@unmsm.edu.pe>
2020-03-03 16:46:39 -0500

    modificacion d

--- ferresol/administracion/views.py
+++ ferresol/administracion/views.py
@@ -12,7 +12,38 @@ def Prueba(request):
 
 def RealizarPago(request):
 
-    return render(request,'realizarPago.html')
+    carros = CarroProducto(request)
+
+    if request.method=='POST':
+        nt = request.POST['nt']
+        if request.POST['cv']=='':
+            cv= 0
+        else:
+            cv = int(request.POST['cv'])
+        fv= request.POST['fv']
+
+        valor = VerificarUsuario(nt,cv,fv)
+        if valor:
+
+            cuenta = CuentaBancaria.objects.get(numero_tarjeta=nt, cv=cv, fecha_vencimiento=fv)
+            coti = Cotizacion.objects.get(id=request.session['cliente'])
+
+            orden= OrdenDeCompra(id_cotizacion=coti,id_cuenta_bancaria=cuenta)
+            orden.save()
+
+            del request.session['cliente']
+
+            return redirect('principal')
+        else:
+            return render(request,'realizarPago.html',{'error':'Informacion de tarjeta no valida'})
+
+
+
+    return render(request,'realizarPago.html',
+                  context={
+                      'carros': carros,
+                  })
+
 
 def DatosEmpresa(request):
 
